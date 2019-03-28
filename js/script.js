2e69d648ff19bd3e434f32a138a93ea146d10375
rodgers <rodgersny99@gmail.com>
2019-03-28 23:04:48 +0300

    changes made

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    //ripples
+    $("#header, .info").ripples({
+        dropRadius: 25,
+        perturbance: 3,
+        
+});
+//magnific popup
+$('.parent-container').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery:{
+        enabled: true
+    }
+    // other options
+  });
+
+
+});
